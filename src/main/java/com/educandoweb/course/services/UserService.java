b6bc65661fd7c387311036b0d1d3d836046b0691
raylon <raylon.f@hotmail.com>
2021-11-27 12:11:22 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -32,4 +32,17 @@ public class UserService {
 	public void delete(Long id) {
 		userRepositories.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		User entity = userRepositories.getOne(id);
+		updateData(entity, obj);
+		return userRepositories.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+	}
 }
