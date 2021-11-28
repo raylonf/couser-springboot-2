562314bcb39a6c033d2709e99f56f4511cd30cf0
raylon <raylon.f@hotmail.com>
2021-11-28 08:06:56 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepositories;
+import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -15,25 +18,31 @@ public class UserService {
 
 	@Autowired
 	private UserRepositories userRepositories;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepositories.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = userRepositories.findById(id);
-		
+
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return userRepositories.save(obj);
 	}
-	
+
 	public void delete(Long id) {
-		userRepositories.deleteById(id);
+		try {
+			userRepositories.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException a ) {
+			throw new DatabaseException(a.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = userRepositories.getOne(id);
 		updateData(entity, obj);
@@ -44,6 +53,6 @@ public class UserService {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
 }
